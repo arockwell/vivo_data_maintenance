6cdc1b5f66c310d6e21bcaece72a748175939491
Alex Rockwell <alexhr@ufl.edu>
2011-04-04 16:50:09 -0400

    Writes results out to different files based on type.

--- find_duplicate_labels.rb
+++ find_duplicate_labels.rb
@@ -6,24 +6,48 @@ require 'vivo_web_api'
 
 require '~/.passwords.rb'
 
-sparql = <<-EOH
-PREFIX rdfs:  <http://www.w3.org/2000/01/rdf-schema#>
+# type is a RDF::URI
+def find_labels_by_type(type)
 
-select ?uri ?label
+  sparql = <<-EOH
+PREFIX rdfs:  <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+select ?uri ?label 
 where
 {
-  ?uri rdfs:label ?label
+  ?uri rdfs:label ?label .
+  ?uri rdf:type <#{type}>
 }
 order by ?uri ?label
-EOH
+  EOH
 
-hostname = ENV['vivo_hostname'] 
-username = ENV['vivo_username']
-password = ENV['vivo_password']
-sparql_client = VivoWebApi::Client.new(hostname)
-sparql_client.authenticate(username, password)
-results = sparql_client.execute_sparql_select(username, password, sparql)
+  hostname = ENV['vivo_hostname'] 
+  username = ENV['vivo_username']
+  password = ENV['vivo_password']
+  sparql_client = VivoWebApi::Client.new(hostname)
+  sparql_client.authenticate(username, password)
+  sparql_client.execute_sparql_select(username, password, sparql)
+end
 
-results.each do |result|
-  puts "#{result[:uri]}\t#{result[:label]}"
+def write_results_to_file(results, type)
+  File.open("label_#{type}.txt", "w") do |f|
+    results.each do |result|
+      f.write("#{result[:uri]}\t#{result[:label]}\n")
+    end
+  end
+end
+
+
+types = [ 
+  {:uri => RDF::URI.new('http://xmlns.com/foaf/0.1/Person'), :name => "person"},
+  {:uri => RDF::URI.new('http://xmlns.com/foaf/0.1/Organization'), :name => "organization"},
+  {:uri => RDF::URI.new('http://vivoweb.org/ontology/core#Grant'), :name => "grant"},
+  {:uri => RDF::URI.new('http://vivoweb.org/ontology/core#Position'), :name => "position"},
+  {:uri => RDF::URI.new('http://vivoweb.org/ontology/core#Role'), :name => "role"}
+]
+
+types.each do |type|
+  results = find_labels_by_type(type[:uri])
+  write_results_to_file(results, type[:name])
 end
--- process_labels.rb
+++ process_labels.rb
@@ -6,25 +6,32 @@ require 'pp'
 
 require '~/.passwords.rb'
 
-uri_label = {}
-File.open('labels.txt').each do |line|
-  (uri, label) = line.split("\t")
-  if uri_label[uri].nil?
-    uri_label[uri] = []
-  end
-  uri_label[uri] << label
-end
+types = %w{person organization role position grant}
 
-uri_label = uri_label.reject do |key, value|
-  if value.size < 2
-    true
-  else
-    false
+types.each do |type|
+  uri_label = {}
+  File.open("label_#{type}.txt").each do |line|
+    (uri, label) = line.chomp.split("\t")
+    if uri_label[uri].nil? 
+      uri_label[uri] = {}
+      uri_label[uri][:label] = []
+    end
+    uri_label[uri][:label] << label
   end
-end
+  uri_label = uri_label.reject do |key, value|
+    if value[:label].size < 2
+      true
+    else
+      false
+    end
+  end
 
-uri_label.each do |uri, labels|
-  labels.each do |label|
-    puts "#{uri}\t#{label}"
+  label_pred = RDF::URI.new("http://www.w3.org/2000/01/rdf-schema#label")
+  RDF::Writer.open("duplicate_label_#{type}.nt") do |writer|
+    uri_label.each do |uri, value|
+      value[:label].each do |label|
+        writer << RDF::Statement(RDF::URI.new(uri), label_pred, label)
+      end
+    end
   end
 end
